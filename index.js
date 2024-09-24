69d412e07d58e55ee0aecbb34f62ea0d9985f751
Bayardo Largaespada <largaespadabayardo88@gmail.com>
2024-09-24 11:47:28 -0600

    Quemen masaya

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-/*const titulo = document.querySelector("h1");
-titulo.innerText = "Productos";
+function envolverCodigo () { const titulo = document.querySelector("h1");
+  titulo.innerText = "Productos";
 
 const Prod = [];
 for (let i = 1; i <= 4; i++) {
@@ -9,13 +9,14 @@ for (let i = 1; i <= 4; i++) {
             <div class="card-body">
                     <h5 class="card-title">Producto ${i}</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary bg-secondary border-0">Agregar</a>
+                    <a href="#" class="btn btn-primary bg-secondary border-0">Ver mas:</a>
             </div>
         </div>`;
 
   Prod.push(Card);
 }
-document.querySelector("section").innerHTML = Prod.join().replaceAll(",", "");*/
+document.querySelector("section").innerHTML = Prod.join().replaceAll(",", "");}
+envolverCodigo();
 
 /*function sacar() {
     for (let i = 1; i <= 4; i++) {
@@ -24,8 +25,36 @@ document.querySelector("section").innerHTML = Prod.join().replaceAll(",", "");*/
 }
 sacar();*/
 
-function suma(numero1, numero2) {
-    return numero1 + numero2;
+/*function suma(numero1, numero2) {
+  return numero1 + numero2;
 }
-let resultado = suma(5,5);
+let resultado = suma(5, 5);
 console.log(resultado);
+
+const loro = (a) => {
+  const b = a + 1;
+  return b;
+};
+console.log(loro(10));*/
+
+function obtenerEstacion() {
+  let dia = parseInt(prompt("Introduce el dia:"));
+  let mes = parseInt(prompt("Introduce el mes:"));
+  let año = parseInt(prompt("Introduce el año:"));
+
+  let estacion;
+  if ((mes == 12 && dia >= 21) || (mes == 1) || (mes == 2) || (mes == 3 && dia < 21)) {
+      estacion = "Invierno";
+  } else if ((mes == 3 && dia >= 21) || (mes == 4) || (mes == 5) || (mes == 6 && dia < 21)) {
+      estacion = "Primavera";
+  } else if ((mes == 6 && dia >= 21) || (mes == 7) || (mes == 8) || (mes == 9 && dia < 21)) {
+      estacion = "Verano";
+  } else if ((mes == 9 && dia >= 21) || (mes == 10) || (mes == 11) || (mes == 12 && dia < 21)) {
+      estacion = "Otoño";
+  }
+
+
+  let resultado = `La fecha ${dia}/${mes}/${año} corresponde a la estacion: ${estacion}`;
+  alert(resultado);
+}
+obtenerEstacion();
