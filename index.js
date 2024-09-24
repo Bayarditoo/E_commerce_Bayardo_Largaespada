23e008411ed9bee350db2ca9ae9ff0846491ffeb
Bayardo Largaespada <largaespadabayardo88@gmail.com>
2024-09-23 19:41:00 -0600

    Prueba de return

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const titulo = document.querySelector("h1");
-titulo.innerText = "Productos"
+/*const titulo = document.querySelector("h1");
+titulo.innerText = "Productos";
 
 const Prod = [];
 for (let i = 1; i <= 4; i++) {
-
-    const Card = `
+  const Card = `
         <div class="card mb-3" style="width: 18rem;">
             <a href="#"><img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" class="card-img-top img-fluid" style="height: 200px; object-fit: cover;" alt="..."></a>
             <div class="card-body">
@@ -12,11 +11,20 @@ for (let i = 1; i <= 4; i++) {
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     <a href="#" class="btn btn-primary bg-secondary border-0">Agregar</a>
             </div>
-        </div>`
+        </div>`;
 
-        Prod.push(Card);
-        }   document.querySelector('section').innerHTML = Prod.join().replaceAll("," , "");
+  Prod.push(Card);
+}
+document.querySelector("section").innerHTML = Prod.join().replaceAll(",", "");*/
 
-const array = [1,2,3];
-    array.push(0,12);
-        console.log(array);
+/*function sacar() {
+    for (let i = 1; i <= 4; i++) {
+        console.log(`Tu Numero es: ${i}`);
+    }
+}
+sacar();*/
+
+function suma(numero1, numero2) {
+    console.log(numero1 % numero2);
+}
+suma(2, 6);
