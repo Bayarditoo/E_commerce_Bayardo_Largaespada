b050ff7008ea78756dc1af00638cccfbe1552beb
Bayardo Largaespada <largaespadabayardo88@gmail.com>
2024-09-24 18:14:09 -0600

    Clase 11

--- index.js
+++ index.js
@@ -37,24 +37,11 @@ const loro = (a) => {
 };
 console.log(loro(10));*/
 
-function obtenerEstacion() {
-  let dia = parseInt(prompt("Introduce el dia:"));
-  let mes = parseInt(prompt("Introduce el mes:"));
-  let año = parseInt(prompt("Introduce el año:"));
+const user = {
+  nombre: `Bayardo`,
+  edad: 16,
+  nacionalidad: `Nicaragua`,
+};
+console.log(user.nombre);
+console.log(user[`edad`]);
 
-  let estacion;
-  if ((mes == 12 && dia >= 21) || (mes == 1) || (mes == 2) || (mes == 3 && dia < 21)) {
-      estacion = "Invierno";
-  } else if ((mes == 3 && dia >= 21) || (mes == 4) || (mes == 5) || (mes == 6 && dia < 21)) {
-      estacion = "Primavera";
-  } else if ((mes == 6 && dia >= 21) || (mes == 7) || (mes == 8) || (mes == 9 && dia < 21)) {
-      estacion = "Verano";
-  } else if ((mes == 9 && dia >= 21) || (mes == 10) || (mes == 11) || (mes == 12 && dia < 21)) {
-      estacion = "Otoño";
-  }
-
-
-  let resultado = `La fecha ${dia}/${mes}/${año} corresponde a la estacion: ${estacion}`;
-  alert(resultado);
-}
-obtenerEstacion();
---
+++ navbar.js
@@ -0,0 +1,5 @@
+let arrayNav = [
+    {texto : `Zapatos`, link: `/zapatos`},
+    {texto : `Camisas`, link: `/remeras`},
+    {texto : `Pantalones`, link: `/pantalones`},
+];
